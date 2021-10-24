8c8c497e6afa4670dc7644e2c0e644f34fc4f478
david-develop <david.peraltar@ab-inbev.com>
2021-10-24 16:36:45 -0500

    login in auth blue print

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,20 @@
 from . import auth
 from app.forms import LoginForm
-from flask import render_template
+from flask import render_template, redirect, url_for, flash, session, request
 
-@auth.route('/login')
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    login_form = LoginForm()
     context = {
         'login_form': LoginForm()
     }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        flash('Nombre de usario registrado con éxito!')
+
+        return redirect(url_for('index'))
     return render_template('login.html', **context)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import request, make_response, redirect, render_template, session, ur
 import unittest
 
 from app import create_app
-from app.forms import LoginForm
+
 
 app = create_app()
 
@@ -37,25 +37,15 @@ def index():
     return response
 
 
-@app.route('/hello', methods=['GET', 'POST'])
+@app.route('/hello', methods=['GET'])
 def hello():
     user_ip = session.get('user_ip')
-    login_form = LoginForm()
     username = session.get('username')
 
     context = {
         'user_ip': user_ip,
         'todos': todos,
-        'login_form': login_form,
         'username': username
     }
-
-    if login_form.validate_on_submit():
-        username = login_form.username.data
-        session['username'] = username
-
-        flash('Nombre de usario registrado con éxito!')
-
-        return redirect(url_for('index'))
 
     return render_template('hello.html', **context)
