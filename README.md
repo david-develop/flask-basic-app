29ee387286cb2ef034fe1eba3bfc50f7fb2e6cae
david-develop <david.peraltar@ab-inbev.com>
2021-11-05 16:57:19 -0500

    firebase connection, simple user model, login/out

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ export FLASK_APP=main.py
 
 - to debbug:
 export FLASK_DEBUG=1
+
+- GCP commands:
+gcloud init -> init the project
+gcloud auth login -> login to gcp
+gcloud auth application-default login -> login localy to interact with cloud from local enviroment
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,9 @@
+from flask_login.utils import login_required, logout_user, login_user
 from . import auth
 from app.forms import LoginForm
 from flask import render_template, redirect, url_for, flash, session, request
+from app.firestore_service import get_user
+from app.models import UserData, UserModel
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -12,9 +15,36 @@ def login():
 
     if login_form.validate_on_submit():
         username = login_form.username.data
-        session['username'] = username
+        password = login_form.password.data
 
-        flash('Nombre de usario registrado con éxito!')
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is not None:
+            password_from_db = user_doc.to_dict()['password']
+
+            if password == password_from_db:
+                user_data = UserData(username, password)
+                user = UserModel(user_data)
+
+                login_user(user)
+
+                flash('Bienvenido de nuevo')
+
+                redirect(url_for('hello'))
+            else:
+                flash('La información no coincide')
+        else:
+            flash('El usuario no existe')
 
         return redirect(url_for('index'))
+
     return render_template('login.html', **context)
+
+
+@auth.route('logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Regresa pronto')
+
+    return redirect(url_for('auth.login'))
